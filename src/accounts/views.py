c219e71e0c8e4b1aa808ae0302581fb20d99cf0e
soumye <singhalsoumye@gmail.com>
2016-08-01 21:13:31 +0530

    folder for accounts

---
+++ src/accounts/views.py
@@ -0,0 +1,71 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+
+    )
+from django.shortcuts import render, redirect
+
+from .forms import UserLoginForm, UserRegisterForm
+
+def login_view(request):
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+    return render(request, "form.html", {"form":form, "title": title})
+
+
+def register_view(request):
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+
+        subject = "email verification"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'faizanurrahimansari@gmail.com']
+        contact_message = "click on the link below to activate your account."
+        send_mail(
+            subject,
+            contact_message,
+            from_email,
+            to_email,
+            fail_silently=False,
+        )
+
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+
+    context = {
+        "form": form,
+        "title": title
+    }
+    return render(request, "form.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
